c3f3355830e4bffec089fbf5578819e0b10d301a
Pierre V <6772696d@gmail.com>
2024-08-15 22:42:06 +0200

    Add operate function to handle every operation with a single function

--- script.js
+++ script.js
@@ -7,14 +7,28 @@ function subtract(n1, n2) {
 	return n1 - n2;
 }
 
-function multiply(n1,n2) {
-    return n1*n2;
+function multiply(n1, n2) {
+	return n1 * n2;
 }
 
-function divide(n1,n2) {
-    // 0 division return 'Infinity'
-    return n1/n2;
+function divide(n1, n2) {
+	// 0 division return 'Infinity'
+	return n1 / n2;
 }
 
 // Inputs
 let inputNumber1, inputNumber2, inputOperator;
+
+// Operate function
+function operate(n1, n2, operator) {
+	switch (operator) {
+		case "+":
+			return add(n1, n2);
+		case "-":
+			return subtract(n1, n2);
+		case "*":
+			return multiply(n1, n2);
+        case "/":
+            return divide(n1,n2);
+	}
+}
